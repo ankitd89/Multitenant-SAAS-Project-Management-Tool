e680d414ddd233d90b929f93629ae754d5ba46bc
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2015-04-29 17:55:22 -0700

    Edited Kanban APIs

--- routes/KanbanApis.js
+++ routes/KanbanApis.js
@@ -3,43 +3,73 @@ var db = require('./db');
 //get requests
 
 this.getProjects = function(req, res, next) {
-    db.dmlQry('insert into registration set ?',registration, function(error,result){
-    if(error){
-        console.log("Error" + error);
-        res.writeHead(500, {'Content-Type': "application/json"});
-        res.end(JSON.stringify({response:error}));
-    }
-    else{
-         res.writeHead(200, {'Content-Type': "application/json"});
-         res.end(JSON.strigify({response:'Saved to MySQL'}));
-    }  
-
-    //return res.render('userProfile',{'firstName':user.firstName,'lastName': user.lastName,'EmailAddress': user._id,'WhatILike': user.whatilike,'Distance':user.Distance});
-        
+   
+	var email_id= req.body.email_id;
+	db.dmlQry('select user_id, tenant_id from Users where email_id = ?',email_id, function(error,result){
+	    if(error){
+	        console.log("Error" + error);
+	        res.writeHead(500, {'Content-Type': "application/json"});
+	        res.end(JSON.stringify({response:error}));
+	    }
+	    user_id=result[0].user_id;
+	    tenant_id = result[0].tenant_id;
+	    db.dmlQry('select distinct(project_name) from Data_table where user_id = ?',user_id, function(error,result){
+		    if(error){
+		        console.log("Error" + error);
+		        res.writeHead(500, {'Content-Type': "application/json"});
+		        res.end(JSON.stringify({response:error}));
+		    }
+		    console.log(result);
+		    var projects = [];
+		    var i=0;
+		    //console.log(result.length);
+		    for(var j=0;j<result.length;j++)
+		    	{
+		    		projects[i]=result[j].project_name;
+		    		 console.log(projects[i]);
+		    		i++;
+		    	}
+		    for(var j=0;j<projects.length;j++)
+	    	{
+		    	var project=projects[j];
+		    	 db.dmlQry('select task_id,task_name,start_date,end_date,record_id from Data_table where project_name = ?',projects[j], function(error,result){
+		 		    if(error){
+		 		        console.log("Error" + error);
+		 		        res.writeHead(500, {'Content-Type': "application/json"});
+		 		        res.end(JSON.stringify({response:error}));
+		 		    }
+		 		    console.log("result    "+result);
+		 		    
+		 		    console.log(result.length);
+		 		    console.log(project);
+		 		   /*var result = [];
+		 		  for (var name in goals) {
+		 		    if (goals.hasOwnProperty(name)) {
+		 		      result.push({name: name, goals: goals[name]});
+		 		    }
+		 		  }*/
+		 		   /* var resu = [];
+		 		    
+		 		    for(var i=0;j<result.length;j++)
+		 		    	{
+		 		    		resu.push({project_task : project, project_tasks : [result[i]]});
+		 		    	}*/
+		 		    var result_array = result;
+		 		    var resUJson = {project_name:project, project_task : result};
+		 		    var Tasks_JSON_array = JSON.stringify(resUJson);
+		 		    console.log(Tasks_JSON_array);
+		 		    
+		    	 });
+	    	}
+		    res.end("hello");
+	    });
 	});
 
  }
 
 
 
- this.getTasks = function(req, res, next) {
-    db.dmlQry('insert into registration set ?',registration, function(error,result){
-    if(error){
-        console.log("Error" + error);
-        res.writeHead(500, {'Content-Type': "application/json"});
-        res.end(JSON.stringify({response:error}));
-    }
-    else{
-         res.writeHead(200, {'Content-Type': "application/json"});
-         res.end(JSON.stringify({response:'Saved to MySQL'}));
-    }  
-
-    return res.render('userProfile',{'firstName':user.firstName,'lastName': user.lastName,'EmailAddress': user._id,'WhatILike': user.whatilike,'Distance':user.Distance});
-        
-	});
-
- }
-
+ 
 //put
 
 this.editTask = function(req, res, next) {
